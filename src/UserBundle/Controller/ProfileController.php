58bdf69e8b7cb840879032e50b7e1b9db1a3b298
goodnesskay <gtkbrain@gmail.com>
2017-09-01 13:36:27 +0100

    create profile controller

---
+++ src/UserBundle/Controller/ProfileController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ProfileController extends Controller
+{
+    public function editProfileAction()
+    {
+
+    }
+}
