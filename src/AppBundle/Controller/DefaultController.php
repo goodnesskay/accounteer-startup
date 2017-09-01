5dbbb3d23d62750260f7e4e1b2ea0e418139ec5d
goodnesskay <gtkbrain@gmail.com>
2017-09-01 06:27:52 +0100

    created layout for app

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $appName = $this->getParameter('app_name');
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'appName' => $appName
         ]);
     }
 }
