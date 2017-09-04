733231ce155cc5c727d2ff4346072c2a648a39fa
goodnesskay <gtkbrain@gmail.com>
2017-09-04 08:28:14 +0100

    create community bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             //...
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
+            new CompanyBundle\CompanyBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/CompanyBundle/CompanyBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CompanyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CompanyBundle extends Bundle
+{
+}
