801e903be3c764ce1d3e083744fcc68ac06aa432
goodnesskay <gtkbrain@gmail.com>
2017-09-04 16:22:51 +0100

    make crud logic

--- src/CompanyBundle/Controller/CompanyController.php
+++ src/CompanyBundle/Controller/CompanyController.php
@@ -5,14 +5,11 @@ namespace CompanyBundle\Controller;
 use CompanyBundle\Entity\Company;
 use CompanyBundle\Form\CompanyType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class CompanyController extends Controller
 {
-    /**
-     * @Route("/company", name="add_company")
-     */
+
     public function createAction(Request $request)
     {
         $user = $this->get('security.token_storage')->getToken()->getUser();
@@ -28,8 +25,13 @@ class CompanyController extends Controller
             $em->persist($company);
             $em->flush();
 
+            $this->get('session')->getFlashBag()->add(
+                'alert',
+                'New Company Added!'
+            );
+
             return $this->redirect($this->generateUrl(
-                'company_list'
+                'company_bundle_list_company'
             ));
         }
 
@@ -38,9 +40,6 @@ class CompanyController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/company/list", name="company_list")
-     */
     public function listAction()
     {
         $userId = $this->get('security.token_storage')->getToken()->getUser();
@@ -50,5 +49,61 @@ class CompanyController extends Controller
         return $this->render('CompanyBundle:Company:list.html.twig',[
             'companies' => $companies
         ]);
+    }
+
+    public function editAction(Request $request,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $company = $em->getRepository('CompanyBundle:Company')->find($id);
+
+        $form = $this->createForm(CompanyType::class,$company);
+        $form->handleRequest($request);
+
+        if (!$company) {
+            throw $this->createNotFoundException(
+                'No Company found for id '.$id
+            );
+        }
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($company);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->add(
+                'alert',
+                'Company Updated!'
+            );
+            return $this->redirect($this->generateUrl('company_bundle_add_edit',[
+                'id' => $id
+            ]));
+        }
+
+        return $this->render('CompanyBundle:Company:edit.html.twig',[
+            'form' => $form->createView(),
+            'company' => $company
+        ]);
+
+    }
+
+    public function deleteAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $company = $em->getRepository('CompanyBundle:Company')->find($id);
+
+        if (!$company) {
+            throw $this->createNotFoundException(
+                'No company found for id '.$id
+            );
+        } else {
+            $em->remove($company);
+            $em->flush();
+            $this->get('session')->getFlashBag()->add(
+                'alert',
+                'Company Deleted!'
+            );
+            return $this->redirect($this->generateUrl('company_bundle_list_company'));
+        }
     }
 }
